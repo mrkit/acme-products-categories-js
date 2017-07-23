402afef4ba50bb6db5c41c9ed330c76f74192dc1
mrkit <neptmanhall@gmail.com>
2017-07-22 21:01:07 -0400

    setup

---
+++ db.js
@@ -0,0 +1,57 @@
+const data = {
+  "Bar Category": [
+    {
+      "name": "bar 1",
+      "id": 1
+    },
+    {
+      "name": "bar 2",
+      "id": 2
+    },
+    {
+      "name": "bar 1",
+      "id": 3
+    },
+    {
+      "name": "gsg",
+      "id": 4
+    }
+  ],
+  "Foo Category": [
+    {
+      "name": "Fubu",
+      "id": 2
+    },
+    {
+      "name": "",
+      "id": 3
+    }
+  ],
+  "": [],
+  "Big Baller Brand": []
+}
+      
+module.exports = {
+  
+  getCategoryNames: function(){
+  
+  },
+  getProductsByCategory: function(){
+  
+  },
+  createProduct: function(){
+    
+  },
+  deleteProduct: function(){
+    
+  },
+  updateProduct: function(){
+    
+  },
+  deleteCategory: function(){
+    
+  },
+  createCategory: function(){
+    
+  }
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const
+  express = require('express'),
+  app = express(),
+  path = require('path'),
+  db = require('./db'),
+  nunjucks = require('nunjucks'),
+  routes = require('./routes/categories');
+
+nunjucks.configure('view', {noCache: true});
+nunjucks.render('index.html', db, function(err, output){
+  if(err) return console.log(err);
+  console.log(output);
+});
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+app.use(require('body-parser').urlencoded({extended:false}));
+app.use(require('method-override')('_method'));
+
+app.get('/', function(req, res, next){
+  res.render('index', {})
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log(`listening on port ${port}`);
+});
